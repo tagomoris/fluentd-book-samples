dd5e72031b8c623d4eeb20c9b03c41a8a9da0982
Satoshi Moris Tagomori <tagomoris@gmail.com>
2022-06-30 16:21:18 +0900

    add a sample code for configurable multi worker support

---
+++ chapter10-4/out_random_path.rb
@@ -0,0 +1,22 @@
+require "fluent/plugin/output"
+
+module Fluent::Plugin
+  class RandomPathOutput < Output
+    Fluent::Plugin.register_output('random_path', self)
+
+    config_param :use_random_file_name, :bool
+
+    def multi_workers_ready?
+      if @use_random_file_name
+        true
+      else
+        false
+      end
+    end
+
+    def write(chunk)
+      file_name = dump_unique_id_hex(generate_unique_id)
+      # file_nameに対して出力
+    end
+  end
+end
